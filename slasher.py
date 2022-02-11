7ea96451f60a327872e06f4cf66c5ad7a4be2d6b
georgeslash <99480609+georgeslash@users.noreply.github.com>
2022-02-11 15:56:34 +0300

    Add files via upload

---
+++ slasher.py
@@ -0,0 +1,8 @@
+#program to login
+username = str(input("Enter username: "))
+password = int(input("Enter password: "))
+ login={"bscit-05-0044/2020",123}
+if username in login and password in login:
+   print("login succesfull")
+else:
+   print("login not succesfull")
---
+++ slasherr.py
@@ -0,0 +1,9 @@
+a=float(input("enter first number"))
+b=float(input("enter first number"))
+c=float(input("enter first number"))
+if a>b and a>c:
+  print("the largest number is",a)
+elif b>a and b>c:
+  print("the largest number is",b)
+else:
+  print("the largest number is",c)
